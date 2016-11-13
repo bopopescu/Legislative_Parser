9e52d89e89192e27c6f4967aef1dbe49ab95206f
David Newswanger <gamma.dave@gmail.com>
2016-11-13 02:21:38 -0500

    added logic to summarize bills

---
+++ app/logic/summarize.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+def summarize(url):
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 10
+
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    # or for plain text files
+    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    out = ""
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        out += str(sentence) + "\n"
+
+    return out
+
+print (summarize("https://www.govtrack.us/congress/bills/114/hr1634/text"))
