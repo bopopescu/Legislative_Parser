22b6e02659200cd6e6f6cd28bcc5a7f782cbabdd
eykrevooh <hooverk@berea.edu>
2016-11-12 19:24:48 -0600

    Added Stuff

--- app/logic/bill_scrapping.py
+++ app/logic/bill_scrapping.py
@@ -3,10 +3,15 @@ import urllib
 
 def get_summaries():
     """Returns each summary in dictionary with the values in keys"""
+    summaries = {}
     r = urllib.urlopen('http://www.govtrack.us/congress/bills/114/s1864/summary')
     soup = BeautifulSoup(r, 'html.parser')
 
     for p in (soup.find_all('p')):
-        print(p.prettify()[0:11])
-
-get_summaries()
+        try:
+            key = int(p.prettify()[11:12])
+            value =  p.get_text()
+            summaries["key"] = value
+        except:
+            pass
+    return summaries
